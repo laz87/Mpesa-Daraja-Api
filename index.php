029306cbbddc72904a96b8a7169f956372df4e01
laz87 <lazaruskariuki87@gmail.com>
2024-11-20 23:12:33 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+// This is a single-line comment
+
+/*
+ This is a multi-line comment
+ You can write comments on multiple lines like this
+*/
+
+// Echo statement outputs the text to the browser
+echo "DARAJA API";
+?>
